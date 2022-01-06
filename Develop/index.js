f77dad7f967b2f940fa6c990742aba036005c8b9
Jennifer Emanuele <jemanuele@gmail.com>
2022-01-06 14:13:58 -0500

    fixed npm issue

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,67 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path')
+const { prompt } = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your project name?'  
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of the project?'
+    },
+    {
+      type: 'input',
+      name: 'contents',
+      message: 'What is your Table of Contents?'  
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are your installations?'  
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage?'  
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'What is your license?'  
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Who is contributing?'  
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the tests?'  
+    },
+   
+    {
+      type: 'input',
+      name: 'questions',
+      message: 'Are there any questions?'  
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    prompt(questions).then(ans=>console.log(ans));
+}
 
 // Function call to initialize app
 init();
